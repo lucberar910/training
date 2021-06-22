c60f768ce90f389028aa4f19c832cb408d9b7d0b
Luca Berardinelli <luca.berardinelli@iquii.com>
2021-06-22 10:32:14 +0200

    primo commit da xcode

---
+++ Training/Shared/TrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainingApp.swift
+//  Shared
+//
+//  Created by Luca Berardinelli on 22/06/21.
+//
+
+import SwiftUI
+
+@main
+struct TrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
