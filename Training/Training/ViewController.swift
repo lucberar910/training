ff33a61b20fdcbd11625fc5fb4cb85ca2dbf60b2
Luca Berardinelli <luca.berardinelli@iquii.com>
2021-06-22 10:42:22 +0200

    progetto iOS

---
+++ Training/Training/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Training
+//
+//  Created by Luca Berardinelli on 22/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
