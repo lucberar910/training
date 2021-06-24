4ad473698d90eb84d6a8fb7a2aa0613cab862a20
Diego Riccardi <diego.riccardi@iquii.com>
2021-06-24 15:14:18 +0200

    usecase

---
+++ Training/Training/Architecture/UseCases/SearchUseCase.swift
@@ -0,0 +1,26 @@
+//
+//  SearchUseCase.swift
+//  Training
+//
+//  Created by Diego Riccardi on 24/06/21.
+//  Copyright (c) 2021 ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+protocol SearchUseCaseProtocol {
+    func execute(text: String, completion: @escaping (Result<[Beer], Error>) -> Void)
+}
+
+class SearchUseCase: SearchUseCaseProtocol {
+    
+    let repository = BeerRepository()
+        
+    
+    
+    func execute(text: String, completion: @escaping (Result<[Beer], Error>) -> Void) {
+        self.repository.search(text, completion: completion)
+    }
+  
+}
